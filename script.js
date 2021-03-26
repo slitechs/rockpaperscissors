52a3dce1da4782bf8ea49b704a330b04a75aef4f
Sophia <slitechs@outlook.com>
2021-03-26 15:56:36 -0700

    Added comments for functions

--- script.js
+++ script.js
@@ -1,58 +1,69 @@
 const SELECTIONS = ["rock", "paper", "scissors"];
 
-      function computerPlay() {
-          let computerSelect = SELECTIONS[Math.floor(Math.random() * SELECTIONS.length)];
-          return computerSelect;
-      }
+/**
+ * Returns computer random selection.
+ *
+ * @param none.
+ * @return {string} A string from a predefined array. 
+ */
+function computerPlay() {
+    let computerSelect = SELECTIONS[Math.floor(Math.random() * SELECTIONS.length)];
+    return computerSelect;
+}
 
-        function playRound(playerSelection, computerSelection) {
-          playerSelection = playerSelection.toLowerCase();
-          if (playerSelection === computerSelection) {
-            return `Tie!`;
-          }
-          else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
-            return `You win!`;
-          }
-          else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
-            return `You lose!`;
-          }
-          else {
-            return "Please select rock, paper, or scissors.";
-          }
-      }
+/**
+ * Returns a string indicating game round result.
+ *
+ * @param {string} playerSelection Player's input.
+ * @param {string} computerSelection Computer's random input.
+ * @return {string} Message of game round result.
+ */
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return `Tie!`;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win!`;
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
+        return `You lose!`;
+    }
+    else {
+        return "Please select rock, paper, or scissors.";
+    }
+}
 
-      function game() {
-        const ROUNDS = 5;
-        let playerChoice = "";
-        let computerChoice = "";
-        let result = "";
-        let playerScore = 0;
-        let computerScore = 0;
-        for (let i = 0; i < ROUNDS; i++) {
-            playerChoice = prompt("Please enter rock, paper, or scissors: ", "");
-            computerChoice = computerPlay();
-            console.log("------ Round: ", i + 1);
-            console.log("Computer: ", computerChoice);
-            result = playRound(playerChoice, computerChoice);
-            console.log(result);
-            // console.log("result.indexOf('win'): ", result.indexOf("win"));
-            // console.log("result.indexOf('lose'): ", result.indexOf("lose"));
-            if (result.indexOf("win") >= 0) {
-                playerScore++;
-            }
-            else if (result.indexOf("lose") >= 0) {
-                computerScore++;
-            }
-            else {
-                // do nothing
-            }
-            console.log(`PLAYER SCORE: `, playerScore, `\r\nCOMPUTER SCORE: `, computerScore);
-          }          
-      }
+/**
+ * Returns none.
+ *
+ * @param none.
+ */
+function game() {
+    const ROUNDS = 5;
+    let playerChoice = "";
+    let computerChoice = "";
+    let result = "";
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < ROUNDS; i++) {
+        playerChoice = prompt("Please enter rock, paper, or scissors: ", "");
+        computerChoice = computerPlay();
+        console.log("------ Round: ", i + 1);
+        console.log("Computer: ", computerChoice);
+        result = playRound(playerChoice, computerChoice);
+        console.log(result);
+        if (result.indexOf("win") >= 0) {
+            playerScore++;
+        }
+        else if (result.indexOf("lose") >= 0) {
+            computerScore++;
+        }
+        else {
+            // do nothing
+        }
+        console.log(`PLAYER SCORE: `, playerScore, `\r\nCOMPUTER SCORE: `, computerScore);
+    }
+}
 
-
-      // const computerSelection = computerPlay();
-      // console.log("Computer: ", computerSelection);
-      // console.log(playRound('ROCk', computerSelection));
-      
-      game();
+game();
